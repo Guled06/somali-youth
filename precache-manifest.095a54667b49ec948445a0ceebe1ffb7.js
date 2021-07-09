10aaa8653d37b0cfb2ca94c0a9d3f10b85635602
Guled Ahmed <akindnesstoremember@gmail.com>
2021-07-09 11:41:41 -0700

    Updates

---
+++ precache-manifest.095a54667b49ec948445a0ceebe1ffb7.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72f8a28202a1f3b4f520dc3a5b3b6091",
+    "url": "/somali-youth/index.html"
+  },
+  {
+    "revision": "f123a090495b7e696425",
+    "url": "/somali-youth/static/css/main.53baeaa3.chunk.css"
+  },
+  {
+    "revision": "e39f08d11c48efe2769e",
+    "url": "/somali-youth/static/js/2.9ea7cb67.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/somali-youth/static/js/2.9ea7cb67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f123a090495b7e696425",
+    "url": "/somali-youth/static/js/main.b034040d.chunk.js"
+  },
+  {
+    "revision": "c9727692ab6ffb5f0450",
+    "url": "/somali-youth/static/js/runtime-main.f072cfc0.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/somali-youth/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/somali-youth/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/somali-youth/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/somali-youth/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/somali-youth/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/somali-youth/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/somali-youth/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/somali-youth/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/somali-youth/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "87e4d053756c60c9f421195d8eafdbfe",
+    "url": "/somali-youth/static/media/features-split-image-03.87e4d053.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/somali-youth/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/somali-youth/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/somali-youth/static/media/logo.2810a88b.svg"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/somali-youth/static/media/video-placeholder.a622fc5d.jpg"
+  }
+]);
